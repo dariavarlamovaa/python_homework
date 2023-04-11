362cf7739b4d82ffa0b53afcd6fe029a7323eb16
Daria <118595398+dariavarlamovaa@users.noreply.github.com>
2023-04-11 20:26:42 +0300

    added new tasks

---
+++ homework py 12/oop_classes.py
@@ -0,0 +1,94 @@
+# task 1
+# class Auto:
+#     def __init__(self):
+#         self.manufacturer = None
+#         self.model = None
+#         self.year = None
+#         self.engine_capacity = None
+#         self.color = None
+#         self.price = None
+#
+#     def fill_it_up(self):
+#         self.manufacturer = input('enter manufacturer`s name: ')
+#         self.model = input('enter car`s model: ')
+#         self.year = input('enter car`s year of release: ')
+#         self.engine_capacity = input('enter car`s engine capacity: ')
+#         self.color = input('enter its color: ')
+#         self.price = input('enter its price: ')
+#
+#     def print_it_out(self):
+#         print(f'manufacturer: {self.manufacturer}\n'
+#               f'model: {self.model}\n'
+#               f'year of release: {self.year}\n'
+#               f'engine capacity: {self.engine_capacity}\n'
+#               f'color: {self.color}\n'
+#               f'price: {self.price}')
+#
+#
+# auto1 = Auto()
+# auto1.fill_it_up()
+# print('______________')
+# auto1.print_it_out()
+
+
+# task 2
+# class Book:
+#     def __init__(self):
+#         self.title = ''
+#         self.year = ''
+#         self.publisher = ''
+#         self.genre = ''
+#         self.author = ''
+#         self.price = ''
+#
+#     def fill_it_up(self):
+#         self.title = input('enter the title of the book: ')
+#         self.author = input('enter autor`s name: ')
+#         self.year = input('enter the year of it`s release: ')
+#         self.publisher = input('enter its publishing house: ')
+#         self.genre = input('enter its genre: ')
+#         self.price = input('enter book`s price: ')
+#
+#     def print_it_out(self):
+#         print(f'title: {self.title}\n'
+#               f'author: {self.author}\n'
+#               f'year of release: {self.year}\n'
+#               f'publishing house: {self.publisher}\n'
+#               f'genre: {self.genre}\n'
+#               f'price: {self.price}')
+#
+#
+# book1 = Book()
+# book1.fill_it_up()
+# print('---------------')
+# book1.print_it_out()
+
+
+# task 3
+# class Stadium:
+#     def __init__(self, name, date_of_opening, country, city, capacity):
+#         self.name = name
+#         self.date_of_opening = date_of_opening
+#         self.country = country
+#         self.city = city
+#         self.capacity = capacity
+#
+#     def fill_it_up(self):
+#         self.name = input('enter the name of stadium: ')
+#         self.date_of_opening = input('enter the opening date: ')
+#         self.country = input('enter the name of the country in which it is located: ')
+#         self.city = input('enter the name of the city in which it is located: ')
+#         self.capacity = input('enter its capacity: ')
+#
+#     def print_it_out(self):
+#         print(f'stadium name: {self.name}\n'
+#               f'opening date: {self.date_of_opening}\n'
+#               f'country: {self.country}\n'
+#               f'city: {self.city}\n'
+#               f'capacity: {self.capacity}\n')
+#
+#
+# stadium1 = Stadium(name='?', date_of_opening='?', country='?', city='?', capacity='?')
+# stadium1.fill_it_up()
+# print('~~~~~~~~~~~~~~~~~~')
+# stadium1.print_it_out()
